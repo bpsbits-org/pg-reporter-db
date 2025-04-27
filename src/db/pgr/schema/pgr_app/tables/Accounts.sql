aa062df4985368a02a6370438fe290f5dbee4278
bpsbits-org <158743628+bpsbits-org@users.noreply.github.com>
2025-04-27 21:57:59 +0300

    Resolved #3
    - table Accounts
    
    Signed-off-by: bpsbits-org <158743628+bpsbits-org@users.noreply.github.com>

---
+++ src/db/pgr/schema/pgr_app/tables/Accounts.sql
@@ -0,0 +1,83 @@
+-- pgr_app.ActiveSessions
+
+-- drop table if exists pgr_app."Accounts";
+-- select * from pgr_app."Accounts";
+
+create table if not exists pgr_app."Accounts"
+(
+    "accountId"         uuid         default pgr_fn."genUuidV7"()                                 not null,
+    "accountLogin"      varchar(128) default pgr_fn."genUuidV7"()::varchar                        not null,
+    "accountLoginHash"  varchar(512) default pgr_fn."toSimpleHash"(pgr_fn."genUuidV7"()::varchar) not null,
+    "accountAttributes" jsonb        default '{}'::jsonb                                          not null,
+    "accountPrivileges" jsonb        default '{}'::jsonb                                          not null,
+    "accountEnabled"    boolean      default true                                                 not null,
+    constraint "pkAccounts"
+        primary key ("accountId"),
+    constraint "uxAccountLogin"
+        unique ("accountLogin")
+);
+
+alter table pgr_app."Accounts"
+    owner to postgres;
+
+comment on table pgr_app."Accounts"
+    is 'pgReporter Accounts';
+
+-- Create before delete trigger function
+create or replace function pgr_app."trgAccountsOnBeforeDelete"()
+    returns trigger
+    language plpgsql
+    security definer
+    immutable
+    cost 1
+as
+$$
+begin
+    raise exception 'Deleting accounts is not allowed. Use accountEnabled flag to disable accounts instead.'
+        using hint = 'Update accountEnabled to FALSE to disable the account',
+            errcode = 'P0001';
+end;
+$$;
+
+comment on function pgr_app."trgAccountsOnAfterUpdate"
+    is 'Prevents account deletion';
+
+-- Create trigger
+create trigger "onBeforeDelete"
+    before delete
+    on pgr_app."Accounts"
+    for each row
+execute function pgr_app."trgAccountsOnBeforeDelete"();
+
+comment on trigger "onBeforeDelete" on pgr_app."Accounts"
+    is 'Prevents account deletion - accounts should be disabled instead of deleted';
+
+-- Create after update trigger function
+create or replace function pgr_app."trgAccountsOnAfterUpdate"()
+    returns trigger
+    language plpgsql
+    security definer
+    volatile
+    parallel unsafe
+    cost 150
+as
+$$
+begin
+    insert into pgr_app."AccountsHistory" ("accountId", "accountData")
+    values (old."accountId", to_jsonb(old));
+    return old;
+end;
+$$;
+
+comment on function pgr_app."trgAccountsOnAfterUpdate"
+    is 'Archives previous account state data after update';
+
+-- Create trigger
+create or replace trigger "onAfterUpdate"
+    after update
+    on pgr_app."Accounts"
+    for each row
+execute function pgr_app."trgAccountsOnAfterUpdate"();
+
+comment on trigger "onAfterUpdate" on pgr_app."Accounts"
+    is 'Archives old state of account';
---
+++ src/db/pgr/schema/pgr_app/tables/AccountsHistory.sql
@@ -0,0 +1,19 @@
+-- pgr_app.AccountsHistory
+
+-- drop table if exists pgr_app."AccountsHistory";
+-- select * from pgr_app."AccountsHistory"
+
+create table pgr_app."AccountsHistory"
+(
+    "accountHistoryId" uuid default pgr_fn."genUuidV7"() not null
+        constraint "pkAccountsHistory"
+            primary key,
+    "accountId"        uuid                              not null,
+    "accountData"      jsonb
+);
+
+comment on table pgr_app."AccountsHistory"
+    is 'Historical records of account.';
+
+alter table pgr_app."AccountsHistory"
+    owner to postgres;
